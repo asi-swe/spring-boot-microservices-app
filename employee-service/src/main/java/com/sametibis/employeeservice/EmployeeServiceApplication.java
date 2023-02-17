9cf18f4f14606ef077c7e55065a827334c196b3d
Samet Ibis <sametibis@Samet-MacBook-Pro.local>
2023-02-16 23:57:57 -0600

    Add WebClient functionality

--- employee-service/src/main/java/com/sametibis/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/sametibis/employeeservice/EmployeeServiceApplication.java
@@ -3,16 +3,18 @@ package com.sametibis.employeeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class EmployeeServiceApplication {
 
+//	@Bean
+//	public RestTemplate restTemplate() {
+//		return new RestTemplate();
+//	}
 	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
+	public WebClient webClient() {
+		return WebClient.builder().build();
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeServiceApplication.class, args);
--- employee-service/src/main/java/com/sametibis/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/sametibis/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -10,10 +10,10 @@ import com.sametibis.employeeservice.mapper.EmployeeMapper;
 import com.sametibis.employeeservice.repository.EmployeeRepository;
 import com.sametibis.employeeservice.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Optional;
 
@@ -22,8 +22,8 @@ import java.util.Optional;
 public class EmployeeServiceImpl implements EmployeeService {
 
     private final EmployeeRepository employeeRepository;
-    private final RestTemplate restTemplate;
-
+    // private final RestTemplate restTemplate;
+    private final WebClient webClient;
     @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
         if(employeeRepository.findEmployeeByEmail(employeeDto.getEmail()).isPresent())
@@ -45,8 +45,18 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public Optional<ApiResponseDto> getEmployeeById(Long id) {
         Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not found!") );
-        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/"+employee.getDepartmentCode(), DepartmentDto.class);
-        DepartmentDto departmentDto = responseEntity.getBody();
+
+        // 1) RestTemplate
+        // ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/"+employee.getDepartmentCode(), DepartmentDto.class);
+        // DepartmentDto departmentDto = responseEntity.getBody();
+
+        // 2) WebClient
+        DepartmentDto departmentDto = webClient.get()
+                .uri("http://localhost:8080/api/departments/"+employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block(); // since I use synchronous communication
+
         EmployeeDto employeeDto = EmployeeMapper.MAPPER.mapToEmployeeDto(employee);
         ApiResponseDto apiResponseDto = new ApiResponseDto(employeeDto, departmentDto);
         return Optional.of(apiResponseDto);
