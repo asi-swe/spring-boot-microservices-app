c15dad95afb77c4e1f76809dbb050fcb5c6477f2
Samet Ibis <sametibis@Samet-MacBook-Pro.local>
2023-02-20 15:18:00 -0600

    Add Spring Cloud Eureka, configure

--- employee-service/src/main/java/com/sametibis/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/sametibis/employeeservice/EmployeeServiceApplication.java
@@ -3,8 +3,6 @@ package com.sametibis.employeeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableFeignClients
